90db807403060a239288a5f31b812c2fe175aeaf
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-04-08 19:04:40 +0530

    Commit

---
+++ src/Advertisement.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Advertisement() {
+  return (
+    <div>Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@ybl .</div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState(0)
+
+  const addNewNumber = () => {
+    Axios.post('https://crud-application-using-mern-stack-for-phonebook-s-backend.vercel.app/add-phone', { name, phone })
+  }
+
+  const [phonebook, setPhonebook] = useState([])
+
+  useEffect(() => {
+    Axios.get('https://crud-application-using-mern-stack-for-phonebook-s-backend.vercel.app/get-phone').then(res => {
+      setPhonebook(res.data.data.phoneNumbers)
+    })
+  }, [])
+
+  const [newPhone, setNewPhone] = useState(0)
+
+  const updatePhone = (id) => {
+    Axios.put('https://crud-application-using-mern-stack-for-phonebook-s-backend.vercel.app/update-phone', { id, newPhone })
+  }
+
+  const deletePhone = (id) => {
+    Axios.delete(`https://crud-application-using-mern-stack-for-phonebook-s-backend.vercel.app/delete-phone/${id}`)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <label htmlFor="">Name: </label>
+      <input type="text" onChange={(e) => { setName(e.target.value) }} /><br /><br />
+      <label htmlFor="">Phone: </label>
+      <input type="number" onChange={(e) => { setPhone(e.target.value) }} /><br /><br />
+
+      <button onClick={addNewNumber}>Add New Number</button>
+
+      <h1>PhoneBook List</h1>
+      {
+        phonebook.map((val, key) => {
+          return <div key={key} className="phone" >
+            <h1>{val.name}</h1>
+            <h1>{val.phone}</h1>
+          </div>
+        })
+      }
+
+      {
+        phonebook.map((val, key) => {
+          return <div key={key} className="phone">
+            <h1>{val.name}</h1>
+            <h1>{val.phone}</h1>
+            <input type="number" placeholder='update Phone...' onChanfe={(e) => {
+              setNewPhone(e.target.value)
+            }} />
+            <button className="update-btn" onClick={() => { updatePhone(val._id) }}>Update</button>
+            <button className='delete-btn' onClick={() => { deletePhone(val._id) }}>Delete</button>
+          </div>
+        })
+      }
+
+
+
     </div>
   );
 }
